12fe4163bd6a4b3c95c9c9fbeaaa4188dabd7625
Alberto Berti <alberto@metapensiero.it>
2015-12-15 17:03:04 +0100

    Initial import from the rocky source tree

---
+++ README.rst
@@ -0,0 +1,18 @@
+.. -*- coding: utf-8 -*-
+.. :Project:   metapensiero.asyncio.transaction -- Handle coroutines from synchronous functions or methods (like special methods)
+.. :Created:   dom 09 ago 2015 12:57:35 CEST
+.. :Author:    Alberto Berti <alberto@metapensiero.it>
+.. :License:   GNU General Public License version 3 or later
+.. :Copyright: Copyright (C) 2015 Alberto Berti
+..
+
+==================================
+ metapensiero.asyncio.transaction
+==================================
+
+Handle coroutines from synchronous functions or methods (like special methods)
+==============================================================================
+
+ :author: Alberto Berti
+ :contact: alberto@metapensiero.it
+ :license: GNU General Public License version 3 or later
---
+++ src/metapensiero/asyncio/transaction/tests/test_transaction.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# :Project:  metapensiero.asyncio.transaction -- tests
+# :Created:    mar 15 dic 2015 15:16:56 CET
+# :Author:    Alberto Berti <alberto@metapensiero.it>
+# :License:   GNU General Public License version 3 or later
+#
+
+import asyncio
+
+import pytest
+
+from metapensiero.asyncio import transaction
+
+@pytest.mark.asyncio
+@asyncio.coroutine
+def test_transaction_per_task(event_loop):
+
+    tasks_ids = set()
+    results = []
+    @asyncio.coroutine
+    def stashed_coro():
+        nonlocal results
+        results.append('called stashed_coro')
+
+    def non_coro_func():
+        tran = transaction.get(loop=event_loop)
+        c = stashed_coro()
+        tran.add(c)
+
+    @asyncio.coroutine
+    def external_coro():
+        nonlocal tasks_ids
+        task = asyncio.Task.current_task(loop=event_loop)
+        tasks_ids.add(id(task))
+        tran = transaction.begin(loop=event_loop)
+        # in py3.5
+        # async with tran:
+        #     non_coro_func()
+        non_coro_func()
+        yield from tran.end()
+
+
+    yield from asyncio.gather(
+        external_coro(),
+        external_coro(),
+        loop=event_loop
+    )
+
+    assert len(tasks_ids) == 2
+    assert len(results) == 2
+    assert results == ['called stashed_coro', 'called stashed_coro']
